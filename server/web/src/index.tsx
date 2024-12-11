4a5f2292975119b6e8dd0e707a2f700990f11d8c
Varakh <varakh@varakh.de>
2024-12-11 18:17:51 +0100

    chore(style): enable strict mode

--- server/web/src/index.tsx
+++ server/web/src/index.tsx
@@ -2,6 +2,7 @@ import './i18n';
 import App from './App';
 import LocaleContextProvider from './providers/LocaleContextProvider';
 import store from './store';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router';
@@ -11,15 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<Provider store={store}>
-		{/* the waring below appears when using ant menu due to react strict mode.
-		 * Using <Button> results in "findDOMNode is deprecated in StrictMode" warning
-		 * Fix not yet available. follow thread: https://github.com/ant-design/ant-design/issues/22493 */}
-		{/*<React.StrictMode>*/}
-		<Router>
-			<LocaleContextProvider>
-				<App />
-			</LocaleContextProvider>
-		</Router>
-		{/*</React.StrictMode>*/}
+		<StrictMode>
+			<Router>
+				<LocaleContextProvider>
+					<App />
+				</LocaleContextProvider>
+			</Router>
+		</StrictMode>
 	</Provider>
 );
