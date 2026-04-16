4813a621cd4f1162096da0f76ab5a96e0fd2d48c
Varakh <gituser@varakh.de>
2026-04-16 23:30:48 +0200

    feat: improve Action History layout in collapsible

--- internal/server/web/src/pages/action-invocations/ItemActionInvocation.tsx
+++ internal/server/web/src/pages/action-invocations/ItemActionInvocation.tsx
@@ -5,7 +5,7 @@ import { ActionInvocationResponse } from '../../types';
 import ActionTextType from '../actions/ActionTextType';
 import EventText from '../events/EventText';
 import { ReloadOutlined } from '@ant-design/icons';
-import { Button, Descriptions, Result, Skeleton, Tooltip, Typography } from 'antd';
+import { Button, Descriptions, Divider, Result, Skeleton, Tooltip, Typography } from 'antd';
 import { FC, ReactNode, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -74,12 +74,17 @@ const ItemActionInvocation: FC<ItemActionInvocationProps> = ({ e }): ReactNode =
 				/>
 			)}
 			{isSuccessAction && dataAction.data && (
-				<Descriptions title={t('action')} layout="vertical" size="small" colon={false}>
-					<Item label={t('label')}>{dataAction.data.label}</Item>
-					<Item label={t('type')}>
-						<ActionTextType type={dataAction.data.type} />
-					</Item>
-				</Descriptions>
+				<>
+					<Divider titlePlacement="start" plain>
+						{t('action')}
+					</Divider>
+					<Descriptions layout="vertical" size="small" colon={false}>
+						<Item label={t('label')}>{dataAction.data.label}</Item>
+						<Item label={t('type')}>
+							<ActionTextType type={dataAction.data.type} />
+						</Item>
+					</Descriptions>
+				</>
 			)}
 			{isLoadingEvent && <Skeleton loading={isLoadingEvent} active={isLoadingEvent} />}
 			{isErrorEvent && (
@@ -101,11 +106,16 @@ const ItemActionInvocation: FC<ItemActionInvocationProps> = ({ e }): ReactNode =
 				/>
 			)}
 			{isSuccessEvent && dataEvent.data && (
-				<Descriptions title={t('event')} layout="vertical" size="small" colon={false}>
-					<Item label={t('name')}>
-						<EventText name={dataEvent.data.name} payload={dataEvent.data.payload} />
-					</Item>
-				</Descriptions>
+				<>
+					<Divider titlePlacement="start" plain>
+						{t('event')}
+					</Divider>
+					<Descriptions layout="vertical" size="small" colon={false}>
+						<Item label={t('name')}>
+							<EventText name={dataEvent.data.name} payload={dataEvent.data.payload} />
+						</Item>
+					</Descriptions>
+				</>
 			)}
 			<Descriptions size="small" layout="vertical" colon={false}>
 				<Item>
