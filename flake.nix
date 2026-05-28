09e85e7ad6b88395425212637a99a56b7687c738
Varakh <gituser@varakh.de>
2026-05-28 17:43:42 +0200

    misc: replace flake-utils and systems with flake-parts

--- flake.nix
+++ flake.nix
@@ -3,81 +3,78 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        version = "8.3.0";
-        frontend = pkgs.stdenv.mkDerivation (finalAttrs: {
-          pname = "upda-ui";
-          inherit version;
-          src = ./internal/server/web;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-          nativeBuildInputs = with pkgs; [
-            nodejs_24
-            pnpm_10
-            pnpmConfigHook
-          ];
+      perSystem =
+        { pkgs, self', ... }:
+        let
+          version = "8.3.0";
+          frontend = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "upda-ui";
+            inherit version;
+            src = ./internal/server/web;
 
-          pnpmInstallFlags = [ "--frozen-lockfile" ];
+            nativeBuildInputs = with pkgs; [
+              nodejs_24
+              pnpm_10
+              pnpmConfigHook
+            ];
 
-          pnpmDeps = pkgs.fetchPnpmDeps {
-            inherit (finalAttrs)
-              pname
-              version
-              src
-              pnpmInstallFlags
-              ;
-            fetcherVersion = 2;
-            hash = "sha256-qpezvwgdKrkvw6/ahJ+PQDQDmwW6LQCv+tJipT5Co/I=";
+            pnpmInstallFlags = [ "--frozen-lockfile" ];
+
+            pnpmDeps = pkgs.fetchPnpmDeps {
+              inherit (finalAttrs)
+                pname
+                version
+                src
+                pnpmInstallFlags
+                ;
+              fetcherVersion = 3;
+              hash = "sha256-DbGib0ayJZirmluQ4D8R7HfcGMKlVLsWBA/grUFIMS8=";
+            };
+
+            buildPhase = ''
+              pnpm build
+              rm -rf build/conf
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out
+              cp -r build $out/
+              runHook postInstall
+            '';
+          });
+        in
+        {
+          packages.frontend = frontend;
+
+          packages.server = pkgs.buildGoModule {
+            pname = "upda";
+            inherit version;
+            src = ./.;
+            tags = [ "prod" ];
+            doCheck = false;
+            vendorHash = "sha256-24UzisWDZon6u59Z+Yf2XB3rNRoEEiyd861cKgZ+f4c=";
+
+            preBuild = ''
+              mkdir -p internal/server/web
+              cp -r ${frontend}/build internal/server/web
+            '';
+            buildInputs = [ frontend ];
           };
 
-          buildPhase = ''
-            pnpm build
-            rm -rf build/conf
-          '';
-
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out
-            cp -r build $out/
-            runHook postInstall
-          '';
-        });
-      in
-      {
-        packages.frontend = frontend;
-
-        packages.server = pkgs.buildGoModule {
-          pname = "upda";
-          inherit version;
-          src = ./.;
-          tags = [ "prod" ];
-          doCheck = false;
-          vendorHash = "sha256-smmCLswh1lmTEFUbm5iMQ8Vh4NGTmWtjOMLgAOVqfww=";
-
-          preBuild = ''
-            mkdir -p internal/server/web
-            cp -r ${frontend}/build internal/server/web
-          '';
-          buildInputs = [ frontend ];
+          packages.default = self'.packages.server;
         };
-
-        packages.default = self.packages.${system}.server;
-      }
-    );
+    };
 }
